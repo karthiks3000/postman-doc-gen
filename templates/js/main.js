0d876e6d07d709c8edf92225c3bb53670446df15
Karthik Subramanian <KSubramanian-consultant@Scholastic.com>
2020-04-03 18:36:19 -0400

    Changes to html template

---
+++ templates/js/main.js
@@ -0,0 +1,65 @@
+jQuery(function ($) {
+
+    // Dropdown menu
+    $(".sidebar-dropdown > a").click(function () {
+        $(".sidebar-submenu").slideUp(200);
+        if ($(this).parent().hasClass("active")) {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this).parent().removeClass("active");
+        } else {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this).next(".sidebar-submenu").slideDown(200);
+            $(this).parent().addClass("active");
+        }
+
+    });
+
+    //toggle sidebar
+    $("#toggle-sidebar").click(function () {
+        $(".page-wrapper").toggleClass("toggled");
+    });
+    //Pin sidebar
+    $("#pin-sidebar").click(function () {
+        if ($(".page-wrapper").hasClass("pinned")) {
+            // unpin sidebar when hovered
+            $(".page-wrapper").removeClass("pinned");
+            $("#sidebar").unbind( "hover");
+        } else {
+            $(".page-wrapper").addClass("pinned");
+            $("#sidebar").hover(
+                function () {
+                    console.log("mouseenter");
+                    $(".page-wrapper").addClass("sidebar-hovered");
+                },
+                function () {
+                    console.log("mouseout");
+                    $(".page-wrapper").removeClass("sidebar-hovered");
+                }
+            )
+
+        }
+    });
+
+
+    //toggle sidebar overlay
+    $("#overlay").click(function () {
+        $(".page-wrapper").toggleClass("toggled");
+    });
+
+
+    //custom scroll bar is only used on desktop
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $(".sidebar-content").mCustomScrollbar({
+            axis: "y",
+            autoHideScrollbar: true,
+            scrollInertia: 300
+        });
+        $(".sidebar-content").addClass("desktop");
+
+    }
+});
+
+
+
+
+
