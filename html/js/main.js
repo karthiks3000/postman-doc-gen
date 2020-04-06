b149a93e80c65cecbbf6e54f1f35a9e7f4ebbf52
Karthik Subramanian <KSubramanian-consultant@Scholastic.com>
2020-04-06 09:26:21 -0400

    changed how the api details are now displayed

--- html/js/main.js
+++ html/js/main.js
@@ -1,22 +1,5 @@
 jQuery(function ($) {
 
-
-
-    // Dropdown menu
-    $(".sidebar-dropdown > a").click(function () {
-        $(".sidebar-submenu").slideUp(200);
-        if ($(this).parent().hasClass("active")) {
-            $(".sidebar-dropdown").removeClass("active");
-            $(this).parent().removeClass("active");
-        } else {
-            $(".sidebar-dropdown").removeClass("active");
-            $(this).next(".sidebar-submenu").slideDown(200);
-            $(this).parent().addClass("active");
-        }
-
-    });
-
-
     //toggle sidebar
     $("#toggle-sidebar").click(function () {
         $(".page-wrapper").toggleClass("toggled");
