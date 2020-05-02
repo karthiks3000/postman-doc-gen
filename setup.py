9f674dacb0a6c749683c1459912cb3bce91fa9ff
Karthik Subramanian <KSubramanian-consultant@Scholastic.com>
2020-05-02 13:53:12 -0400

    Changes to generate an exe

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='postman_doc_gen',
+
+      version='1.0.0',
+
+      url='https://github.com/karthiks3000/postman-doc-gen.git',
+
+      license='MIT',
+
+      author='Karthik Subramanian',
+
+      author_email='karthiks3000@yahoo.com',
+
+      description='Converts a postman collection into an HTML documentation',
+
+      packages=find_packages(exclude=['tests']),
+
+      long_description=open('README.md').read(),
+
+      zip_safe=False,
+
+      setup_requires=['fastjsonschema>=2.14', 'Jinja2>=2.11', 'MarkupSafe>=1.1'],
+      python_requires='>=3.0')
